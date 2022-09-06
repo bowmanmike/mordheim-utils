45a5371953dbee5e098713015048c4214eb96ae1
Mike Bowman <mbowman2@gmail.com>
2022-09-06 17:20:42 -0400

    Add collapsing sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,39 @@
-<section class="mb-4">
-	<h3 class="font-semibold text-xl text-center">Ranged Weapons To Hit</h3>
-	<table class="w-full">
-		<tr class="border-gray-800 border-b">
-			<td class="font-bold">BS Of Shooter</td>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
-			<td>5</td>
-			<td>6</td>
-			<td>7</td>
-			<td>8</td>
-			<td>9</td>
-			<td>10</td>
-		</tr>
-		<tr>
-			<td>D6 Roll Needed</td>
-			<td>6</td>
-			<td>5</td>
-			<td>4</td>
-			<td>3</td>
-			<td>2</td>
-			<td>1</td>
-			<td>0</td>
-			<td>-1</td>
-			<td>-2</td>
-			<td>-3</td>
-		</tr>
-	</table>
-</section>
+<script lang="ts">
+	import CollapsibleSection from '$lib/CollapsibleSection.svelte';
+</script>
 
-<section class="mb-4">
-	<h3 class="font-semibold text-xl text-center">Ranged Hit Modifiers</h3>
+<CollapsibleSection title={"Ranged Weapons To Hit"}>
+		<table class="w-full">
+			<tr class="border-gray-800 border-b">
+				<td class="font-bold">BS Of Shooter</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>9</td>
+				<td>10</td>
+			</tr>
+			<tr>
+				<td>D6 Roll Needed</td>
+				<td>6</td>
+				<td>5</td>
+				<td>4</td>
+				<td>3</td>
+				<td>2</td>
+				<td>1</td>
+				<td>0</td>
+				<td>-1</td>
+				<td>-2</td>
+				<td>-3</td>
+			</tr>
+		</table>
+</CollapsibleSection>
+
+<CollapsibleSection title="Ranged Hit Modifiers">
 	<table>
 		<tr>
 			<td>-1</td>
@@ -54,10 +56,9 @@
 			<td>If the target is labelled as Large, or whose main body is over 2" tall or wide</td>
 		</tr>
 	</table>
-</section>
+</CollapsibleSection>
 
-<section class="mb-4">
-	<h3 class="font-semibold text-xl text-center">Close Combat To Hit</h3>
+<CollapsibleSection title="Close Combat To Hit">
 	<div class="flex items-center">
 		<p>Attacker's Weapon Skill</p>
 		<table class="table-auto w-full">
@@ -207,10 +208,9 @@
 			</tr>
 		</table>
 	</div>
-</section>
+</CollapsibleSection>
 
-<section>
-	<h3 class="font-semibold text-xl text-center">Wound Chart</h3>
+<CollapsibleSection title="Wound Chart">
 	<div class="flex items-center">
 		<p>Weapon or Attacker's Strength</p>
 		<table class="table-auto w-full">
@@ -360,7 +360,7 @@
 			</tr>
 		</table>
 	</div>
-</section>
+</CollapsibleSection>
 
 <style>
 	p {
