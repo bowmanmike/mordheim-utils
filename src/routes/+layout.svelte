0b42ed074734dac7f0744ab4fad5eb9f651c20eb
Mike Bowman <mbowman2@gmail.com>
2022-09-06 15:56:25 -0400

    Add wound/hit charts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,17 @@
 	import '../app.css';
 </script>
 
-<header class="flex justify-between">
-	<div><h1>Mordheim Utils</h1></div>
-	<nav>
-		<ul>
-			<li>Home</li>
-			<li>Charts & Tables</li>
-			<li>Warbands</li>
-		</ul>
-	</nav>
+<header class="flex justify-between mb-4">
+	<div class="w-full">
+		<h1 class="font-bold text-2xl w-full text-center">Mordheim Helper</h1>
+	</div>
+	<!-- <nav> -->
+	<!-- 	<ul> -->
+	<!-- 		<li>Home</li> -->
+	<!-- 		<li>Charts & Tables</li> -->
+	<!-- 		<li>Warbands</li> -->
+	<!-- 	</ul> -->
+	<!-- </nav> -->
 </header>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,343 @@
-<p>Mordheim Stuff goes here</p>
+<section class="mb-4">
+	<h3 class="font-semibold text-xl text-center">Ranged Weapons To Hit</h3>
+	<table class="w-full">
+		<tr class="border-gray-800 border-b">
+			<td class="font-bold">BS Of Shooter</td>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td>10</td>
+		</tr>
+		<tr>
+			<td>D6 Roll Needed</td>
+			<td>6</td>
+			<td>5</td>
+			<td>4</td>
+			<td>3</td>
+			<td>2</td>
+			<td>1</td>
+			<td>0</td>
+			<td>-1</td>
+			<td>-2</td>
+			<td>-3</td>
+		</tr>
+	</table>
+</section>
+
+<section class="mb-4">
+	<h3 class="font-semibold text-xl text-center">Close Combat To Hit</h3>
+	<div class="flex items-center">
+		<p>Attacker's Weapon Skill</p>
+		<table class="table-auto w-full">
+			<caption>Defender's Weapon Skill</caption>
+			<tr>
+				<td>X</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>9</td>
+				<td>10</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>1</td>
+				<td>4</td>
+				<td>4</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+				<td>5</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>5</td>
+				<td>5</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>5</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>7</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+				<td>4</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>9</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+				<td>4</td>
+			</tr>
+			<tr>
+				<td>10</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>3</td>
+				<td>4</td>
+			</tr>
+		</table>
+	</div>
+</section>
+
+<section>
+	<h3 class="font-semibold text-xl text-center">Wound Chart</h3>
+	<div class="flex items-center">
+		<p>Weapon or Attacker's Strength</p>
+		<table class="table-auto w-full">
+			<caption>Target's Toughness</caption>
+			<tr>
+				<td>X</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>9</td>
+				<td>10</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>1</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>3</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>5</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+				<td>-</td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+				<td>-</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>7</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>6</td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+			</tr>
+			<tr class="bg-gray-200">
+				<td>9</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+			</tr>
+			<tr>
+				<td>10</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+			</tr>
+		</table>
+	</div>
+</section>
+
+<style>
+	p {
+		writing-mode: vertical-lr;
+	}
+</style>
