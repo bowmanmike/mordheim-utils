58493181df17c9caacf5abb5222e8ff56abd3034
Mike Bowman <mbowman2@gmail.com>
2022-09-06 13:26:10 -0400

    Add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="flex justify-between">
+	<div><h1>Mordheim Utils</h1></div>
+	<nav>
+		<ul>
+			<li>Home</li>
+			<li>Charts & Tables</li>
+			<li>Warbands</li>
+		</ul>
+	</nav>
+</header>
+
+<slot />
