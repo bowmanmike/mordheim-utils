79d0e7cccd946e14c3295a4b3d89e0a01a4f5050
Mike Bowman <mbowman2@gmail.com>
2022-09-19 18:10:22 -0400

    Big reorg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,37 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+
+	const routes = [
+		{ href: '/', text: 'Home' },
+		{ href: '/tables', text: 'Charts & Tables' }
+	];
+
+	$: activePage = $page.url.pathname;
 </script>
 
-<header class="flex justify-between mb-4">
-	<div class="w-full">
-		<h1 class="font-bold text-2xl w-full text-center">Mordheim Helper</h1>
+<header class="mb-4">
+	<div class="w-full mb-4">
+		<h1 class="font-bold text-2xl w-full text-center"><a href="/">Mordheim Helper</a></h1>
 	</div>
 	<!-- <nav> -->
-	<!-- 	<ul> -->
-	<!-- 		<li>Home</li> -->
-	<!-- 		<li>Charts & Tables</li> -->
-	<!-- 		<li>Warbands</li> -->
-	<!-- 	</ul> -->
+		<!-- <ul class="flex items-center justify-center gap-2"> -->
+			<!-- {#each routes as route} -->
+				<!-- <li> -->
+					<!-- <a -->
+						<!-- href={route.href} -->
+						<!-- class={`${ -->
+							<!-- activePage === route.href && 'border-2 border-gray-800' -->
+						<!-- } bg-gray-300 rounded py-2 px-4`}>{route.text}</a -->
+					<!-- > -->
+				<!-- </li> -->
+			<!-- {/each} -->
+		<!-- </ul> -->
 	<!-- </nav> -->
 </header>
 
 <slot />
+
+<!-- <footer class="text-center my-10 min-w-full"> -->
+	<!-- &copy; 2022 &mdash; Mike Bowman &mdash; <a href="https://mikebowman.dev">mikebowman.dev</a> -->
+<!-- </footer> -->
---
+++ src/routes/tables/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Tables from '$lib/Tables.svelte';
+</script>
+
+<Tables />
